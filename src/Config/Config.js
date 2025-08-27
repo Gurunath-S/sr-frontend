b823bc5acd4a210d07916e71037f27c7306e401a
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-08-27 14:58:06 +0530

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,2 +1,2 @@
-export const BACKEND_SERVER_URL ="https://sr-jewellery-b.onrender.com"
+export const BACKEND_SERVER_URL ="https://sr-backend-u6ly.onrender.com"
   // import.meta.env.VITE_BACKEND_SERVER_URL || "http://localhost:5002";
