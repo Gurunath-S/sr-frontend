23f77e085dba6aad56b06a7be81a99490f8ac113
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-08-27 14:57:01 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch('https://sr-jewellery-b.onrender.com/api/auth/login', {
+      const response = await fetch(`${BACKEND_SERVER_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
